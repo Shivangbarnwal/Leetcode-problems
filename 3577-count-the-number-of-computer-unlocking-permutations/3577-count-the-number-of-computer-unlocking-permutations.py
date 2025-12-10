ea9e25a89caf4066125f95ea45f4eec1c35653b9
Shivangbarnwal <shivangb10074@gmail.com>
2025-12-10 11:11:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countPermutations(self, complexity):
+        """
+        :type complexity: List[int]
+        :rtype: int
+        """
+        n=len(complexity)
+        if complexity[0]==min(complexity) and complexity.count(min(complexity))==1:
+            c=1
+            while n!=1:
+                n-=1
+                c*=n
+            return c
+        return 0
+        
