683a2c8625f7c513616cdf384de65d8da79d2562
Shivangbarnwal <shivangb10074@gmail.com>
2026-01-02 11:35:12 +0530

    Time: 0 ms (100%), Space: 13.3 MB (61.54%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -4,10 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nuke=[]
-        for kim in nums:
-            if kim in nuke:
-                return kim
-            else:
-                nuke.append(kim)
-                
+        for i in range(len(nums)-2):
+            if nums[i]==nums[i+1] or nums[i]==nums[i+2]:
+                return nums[i]
+        
+        if nums[0]==nums[-1] or nums[-1]==nums[-2]:
+            return nums[-1]                
