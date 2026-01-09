e69ad44cc105574a52d929dd945a42308565fcba
Shivangbarnwal <shivangb10074@gmail.com>
2026-01-09 17:05:15 +0530

    Time: 507 ms (43.29%), Space: 13.4 MB (31.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,7 +6,9 @@ class Solution(object):
         :rtype: List[int]
         """
         n=len(nums)
-        for i in range(n-1):
-            for j in range(i+1,n):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
+        hashmap={}
+        for i in range(n):
+            if nums[i] in hashmap.keys():
+                return i,hashmap[nums[i]]
+            hashmap[target-nums[i]]=i
+        
