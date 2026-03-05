93cfec5e75427ee43532552cd20aae10a50600b2
Shivangbarnwal <shivangb10074@gmail.com>
2026-03-05 11:28:15 +0530

    Time: 55 ms (14.21%), Space: 20.9 MB (36.49%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        arr=[]
-        preprod=1
-        for i in range(len(nums)):
-            arr.append(preprod)
-            preprod*=nums[i]
-        suff=1
-        for i in range(1,len(nums)+1):
-            arr[-i]*=suff
-            suff*=nums[-i]
-        return arr
+        n=len(nums)
+        pre=[1]*n
+        suf=[1]*n
+        for i in range(n-1):
+            pre[i+1]*=nums[i]*pre[i]
+            suf[-i-2]*=nums[-i-1]*suf[-i-1]
+        
+        for j in range(n):
+            pre[j]*=suf[j]
+        return pre
+
         
