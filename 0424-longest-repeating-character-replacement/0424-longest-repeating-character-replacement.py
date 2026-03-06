860deeede963f40231af3bc2dc666d0a03bb14cf
Shivangbarnwal <shivangb10074@gmail.com>
2026-03-06 12:11:26 +0530

    Time: 169 ms (31.78%), Space: 15.3 MB (73.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,17 @@
 class Solution(object):
     def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        freq={}
-        left=0
+        mpp={}
         maxcount=0
         maxlen=0
+        left=0
         for right in range(len(s)):
-            if s[right] in freq.keys():
-                freq[s[right]]+=1
+            if s[right] in mpp.keys():
+                mpp[s[right]]+=1
             else:
-                freq[s[right]]=1
-            maxcount=max(maxcount,freq[s[right]])
+                mpp[s[right]]=1
+            maxcount=max(maxcount,mpp[s[right]])
             while (right-left+1)-maxcount>k:
-                freq[s[left]]-=1
+                mpp[s[left]]-=1
                 left+=1
             maxlen=max(maxlen,right-left+1)
         return maxlen
-        
