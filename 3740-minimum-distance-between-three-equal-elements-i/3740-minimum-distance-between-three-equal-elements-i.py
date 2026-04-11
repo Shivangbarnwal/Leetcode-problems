5fa0cf05da0ed718a0c85ca5a87aa92563351cad
Shivangbarnwal <shivangb10074@gmail.com>
2026-04-11 16:41:01 +0530

    Time: 4 ms (75%), Space: 12.3 MB (95%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minimumDistance(self, nums):
+        
+        def check(mindis,arr):
+            for i in range(len(arr)-2):
+                mindis=min(mindis,2*(arr[i+2]-arr[i]))
+            return mindis
+        mpp=defaultdict(list)
+        for j in range(len(nums)):
+            mpp[nums[j]].append(j)
+        mindis=float('inf')
+        for k in mpp.keys():
+            if len(mpp[k])>=3:
+                mindis=check(mindis,mpp[k])
+        if mindis==float('inf'):
+            return -1
+        else:
+            return mindis
