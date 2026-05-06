324944260f721a80b8b5952d0bba0540f9ba1c81
Shivangbarnwal <shivangb10074@gmail.com>
2026-05-06 08:24:24 +0530

    Time: 157 ms (83.72%), Space: 53.7 MB (41.86%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -4,27 +4,21 @@ class Solution(object):
         :type boxGrid: List[List[str]]
         :rtype: List[List[str]]
         """
-        mat=[]
         
         n=len(boxGrid)
         m=len(boxGrid[0])
-
-        for i in range(n):
-            temp=["."]*(m)
-            bot=0
-            for j in range(m):
-                k=boxGrid[i][-j-1]
-                if k=="#":
-                    temp[bot]=k
-                    bot+=1
-                elif k=="*":
-                    temp[j]=k
-                    bot=j+1
-            mat.insert(0,temp[::-1])
         ans=[["." for _ in range(n)] for _ in range(m)]
         for i in range(n):
-            for j in range(m):
-                ans[j][i]=mat[i][j]
+            bot=m-1
+            for j in range(m-1,-1,-1):
+                
+                if boxGrid[i][j]=="*":
+                    ans[j][n-i-1]="*"
+                    bot=j-1
+                elif boxGrid[i][j]=="#":
+                    ans[bot][n-1-i]="#"
+                    bot-=1
+           
         return ans
 
         
