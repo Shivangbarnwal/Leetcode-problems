10c4abf62db7118d1c3a3021badd7a37abd1aa93
Shivangbarnwal <shivangb10074@gmail.com>
2026-05-20 17:53:42 +0530

    Time: 0 ms (100%), Space: 12.9 MB (65.2%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        right=len(nums)-1
+        left=0
+        idx=0
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] < target:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+        return left
+
+
+
+        
