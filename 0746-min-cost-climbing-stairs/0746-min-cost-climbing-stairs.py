31f85005bbec6d3a5d03806be65909903cb6dcbc
Shivangbarnwal <shivangb10074@gmail.com>
2026-06-06 17:13:32 +0530

    Time: 4 ms (47.6%), Space: 12.4 MB (61.4%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -5,8 +5,11 @@ class Solution(object):
         :rtype: int
         """
         n=len(cost)
-        dp=[0]*(n+1)
+        pre1,pre2=0,0
         for i in range(2,n+1):
-            dp[i]=min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2])
-        return dp[n]
+
+            cur=min(cost[i-1]+pre1,cost[i-2]+pre2)
+            pre2=pre1
+            pre1=cur
+        return pre1
         
