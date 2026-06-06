024e11a51c3eff9af15afcbf466428789db21c0e
Shivangbarnwal <shivangb10074@gmail.com>
2026-06-06 17:11:36 +0530

    Time: 3 ms (71.06%), Space: 12.3 MB (89.91%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -5,15 +5,8 @@ class Solution(object):
         :rtype: int
         """
         n=len(cost)
-        dp=[-1]*(n+1)
-        def climb(cost,n):
-            if dp[n]!=-1:
-                return dp[n]
-            elif n in [0,1]:
-                dp[n]=0
-                return dp[n]
-            else:
-                dp[n]=min(cost[n-1]+climb(cost,n-1),cost[n-2]+climb(cost,n-2))
-                return dp[n]
-        return climb(cost,n)
+        dp=[0]*(n+1)
+        for i in range(2,n+1):
+            dp[i]=min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2])
+        return dp[n]
         
