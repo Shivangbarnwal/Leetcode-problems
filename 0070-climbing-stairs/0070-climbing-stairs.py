3fb64da6ec29ffb870b8ae0502bd7675012302e8
Shivangbarnwal <shivangb10074@gmail.com>
2026-06-06 16:59:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (20.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,12 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp=[-1]*(n+1)
-        dp[0]=1
-        dp[1]=1
+        prev2=1
+        prev=1
         for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+            cur=prev+prev2
+            prev2=prev
+            prev=cur
+        return prev
 
 
         
