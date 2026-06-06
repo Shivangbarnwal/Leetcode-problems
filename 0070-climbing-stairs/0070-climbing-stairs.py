95d4e1e434859cc1ddf43f3e8ba29728d8f6d154
Shivangbarnwal <shivangb10074@gmail.com>
2026-06-06 16:58:01 +0530

    Time: 0 ms (100%), Space: 12.3 MB (56.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,16 +5,11 @@ class Solution(object):
         :rtype: int
         """
         dp=[-1]*(n+1)
-        
-        def ways(n):
-            if n<=1:
-                return 1
-            if dp[n]!=-1:
-                return dp[n]
-            else:
-                dp[n]=ways(n-1)+ways(n-2)
-                return dp[n]
-        return ways(n)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
 
 
         
