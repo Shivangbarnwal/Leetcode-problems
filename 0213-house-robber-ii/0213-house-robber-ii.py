e8fc150bec3f8dcbd853d8f307c1f4ff51d38133
Shivangbarnwal <shivangb10074@gmail.com>
2026-07-26 13:04:17 +0530

    Time: 3 ms (7.2%), Space: 19.2 MB (92.76%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,15 +1,13 @@
-class Solution(object):
-    def rob(self, nums):
-        n=len(nums)-1
-        def robber(n,nums):
-            n=len(nums)
-            pre2=0
-            pre1=nums[0]
-            for i in range(1,n):
-                cur=max(nums[i]+pre2,pre1)
-                pre2=pre1
-                pre1=cur
-            return pre1
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if len(nums)==1:
             return nums[0]
-        return max(robber(n-1,nums[1:]),robber(n-1,nums[:n]))
+        pre1,pre2=0,0
+        for i in nums[1:]:
+            pre1,pre2=max(pre1,pre2+i),pre1
+        m=pre1
+        pre1,pre2=0,0
+        for j in range(len(nums)-1):
+            pre1,pre2=max(pre1,pre2+nums[j]),pre1
+        m=max(m,pre1)
+        return m
