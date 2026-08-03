4adf18c6317f804e12a71c84dddb29eb590ccc6f
Shivangbarnwal <shivangb10074@gmail.com>
2026-08-03 08:18:19 +0530

    Time: 3 ms (2.06%), Space: 19.3 MB (17.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,7 +7,7 @@ class Solution:
                 return 0
             if dp[n]!=-1:
                 return dp[n]
-            if n<=1:
+            if n<=0:
                 dp[n]=1
                 return 1
             dp[n]= rec(n-1)+rec(n-2)
